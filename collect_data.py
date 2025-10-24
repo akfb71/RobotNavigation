543dad2e4fbb6b5f767264843697d01faa64c9e0
akfb71 <akfb71@gmail.com>
2025-10-24 14:12:17 -0500

    Fix comments for clarity in collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -15,7 +15,7 @@ def collect_training_data(total_actions):
     steering_behavior = Wander(action_repeat)
 
     num_params = 7
-    #STUDENTS: network_params will be used to store your training data
+    #network_params will be used to store your training data
     # a single sample will be comprised of: sensor_readings, action, collision
     network_params = np.zeros((total_actions, num_params))
 
@@ -34,18 +34,18 @@ def collect_training_data(total_actions):
 
             if collision:
                 steering_behavior.reset_action()
-                #STUDENTS NOTE: this statement only EDITS collision of PREVIOUS action
+                #this statement only EDITS collision of PREVIOUS action
                 #if current action is very new.
                 if action_timestep < action_repeat * .3: #in case prior action caused collision
                     network_params[-1][-1] = collision #share collision result with prior action
                 break
 
-        #STUDENTS: Update network_params.
+        #Update network_params.
         network_params[action_i, :] = np.concatenate((sensor_readings, [action,collision]))
     with open("submission.csv", mode="w", newline="") as file:
         writer = csv.writer(file)
         writer.writerows(network_params.tolist())
-        #STUDENTS: Save .csv here. Remember rows are individual samples, the first 5
+        #Save .csv here. Remember rows are individual samples, the first 5
     #columns are sensor values, the 6th is the action, and the 7th is collision.
     #Do not title the columns. Your .csv should look like the provided sample.
 
