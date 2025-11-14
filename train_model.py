a0243d5e90a61e27cb20f6095b388845276fae49
akfb71 <fatimabasheer1234567@gmail.com>
2025-11-14 11:02:43 -0600

    Training with basic NN and no optimizations

---
+++ train_model.py
@@ -0,0 +1,30 @@
+from Data_Loaders import Data_Loaders
+from Networks import Action_Conditioned_FF
+
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+
+def train_model(no_epochs):
+
+    batch_size = 10
+    data_loaders = Data_Loaders(batch_size)
+    model = Action_Conditioned_FF()
+
+
+    losses = []
+    min_loss = model.evaluate(model, data_loaders.test_loader, nn.BCEWithLogitsLoss())
+    losses.append(min_loss)
+
+
+    for epoch_i in range(no_epochs):
+        model.train()
+        for idx, sample in enumerate(data_loaders.train_loader): # sample['input'] and sample['label']
+            pass
+
+
+
+if __name__ == '__main__':
+    no_epochs = 20
+    train_model(no_epochs)
